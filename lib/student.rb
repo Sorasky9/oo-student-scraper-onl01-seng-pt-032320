62204e844d71442cfbe1f1a1c02527d959a964a6
Malcom Fetus <sora.vista@gmail.com>
2020-05-23 22:34:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |key, value|
+    students_array.each do |key|
       binding.pry
   end
   end
