1fc19b7d9382a93b171dd70e8d8cb3d0845a258f
Malcom Fetus <sora.vista@gmail.com>
2020-05-23 23:43:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do {|key, value| self.send (("#{key}="))}
+    attributes_hash.each do {|key, value| self.send (("#{key}="), value)}
     #binding.pry
 
 
