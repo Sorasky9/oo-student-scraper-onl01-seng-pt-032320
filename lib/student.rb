5e7ab6c28ca0e63002c2babe10fbc5c447c51156
Malcom Fetus <sora.vista@gmail.com>
2020-05-23 23:28:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   def self.create_from_collection(students_array)
     #<-students_array is a list of hashes
     students_array.each do |key| #<- key brings only the name and location of the hash
-      Student.new(key)
+      Student.new(key) #<- Student class instantiate specifically of the name inside hash called key from the iteration above.
 
 
       #binding.pry
